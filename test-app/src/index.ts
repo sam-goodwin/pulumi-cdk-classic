c486f8693356d05459da636fe107e63c01cb57b0
sam <sam@functionless.org>
2023-01-23 16:04:07 +1100

    fix target

--- test-app/src/index.ts
+++ test-app/src/index.ts
@@ -1,7 +1,7 @@
 // import * as cdk from "@pulumi/cdk";
 // import { remapCloudControlResource } from "pulumi-l2";
 // import { aws_lambda } from "aws-cdk-lib";
-import { iam, lambda } from "@pulumi/aws";
+import { iam } from "@pulumi/aws";
 
 const role = new iam.Role("my-role", {
   assumeRolePolicy: {
